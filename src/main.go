7fee93a21e65276ad995022116cd0d8fb82b19f1
Eatswap <root@eatswap.org>
2022-08-26 22:46:13 +0800

    add: sample content

--- src/main.go
+++ src/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"runtime"
+	"time"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"GOOS":         runtime.GOOS,
+			"GOARCH":       runtime.GOARCH,
+			"Version":      runtime.Version(),
+			"NumCPU":       runtime.NumCPU(),
+			"NumGoroutine": runtime.NumGoroutine(),
+			"NumCgoCall":   runtime.NumCgoCall(),
+			"Time":         time.Now().Format(time.RFC1123),
 		})
 	})
-	r.Run(":8080") // listen and serve on :8080
+	r.Run(":10000") // listen and serve on :8080
 }
