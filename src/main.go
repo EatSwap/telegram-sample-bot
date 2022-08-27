589e1488339ebc63edcb4fac2986ac4160514832
Eatswap <root@eatswap.org>
2022-08-27 23:27:36 +0800

    add: SIGINT

--- src/main.go
+++ src/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
 	"runtime"
 	"time"
 )
 
 func main() {
+	// Handle SIGINT
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			// sig is a ^C, handle it
+			fmt.Printf("\nReceived an interrupt, stopping services...\n")
+			os.Exit(0)
+		}
+	}()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -19,5 +33,5 @@ func main() {
 			"Time":         time.Now().Format(time.RFC1123),
 		})
 	})
-	r.Run(":10000") // listen and serve on :8080
+	r.Run(":8080") // listen and serve on :8080
 }
